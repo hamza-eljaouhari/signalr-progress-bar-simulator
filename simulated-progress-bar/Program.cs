06ccbb34188e281f7ced0349f27767ac222dcec7
Hamza <hamza.eljoauhari.1995@gmail.com>
2023-12-16 20:56:01 +0100

    Initial Commit

---
+++ simulated-progress-bar/Program.cs
@@ -0,0 +1,47 @@
+﻿using simulated_progress_bar.Signals;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add SignalR services
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000") // Your client app's URL
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Use CORS policy
+app.UseCors("CorsPolicy");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Map SignalR hubs
+app.MapHub<ProgressHub>("/progressHub"); // Ensure you have a ProgressHub class
+
+app.MapControllers();
+
+app.Run();
---
+++ simulated-progress-bar/Signals/ProgressHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace simulated_progress_bar.Signals
+{
+
+    public class ProgressHub : Hub
+    {
+        public async Task StartTask()
+        {
+            Random rnd = new Random();
+            int taskDuration = rnd.Next(10, 40); // Duration in seconds (40 to 120)
+            int elapsed = 0;
+
+            while (elapsed < taskDuration)
+            {
+                await Task.Delay(1000); // Wait for 1 second
+                elapsed++;
+                int progress = (int)((float)elapsed / taskDuration * 100);
+                await Clients.Caller.SendAsync("ReceiveProgress", progress);
+            }
+        }
+    }
+
+}
+
